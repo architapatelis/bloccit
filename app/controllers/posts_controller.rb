fc8b9045b6af9bbcb0d6ef14f5c74a837102f6a6
Archita <architapatelis@gmail.com>
2017-04-13 13:33:58 -0700

    first and every fifth post title is spam

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,11 @@ class PostsController < ApplicationController
     #declare instance variable 'posts' and assign it a array of Post objects using the all method provided by ActiveRecord.
     # instance variables created in controller method are available for use in associated view
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
