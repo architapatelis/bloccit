ed022c9cfe78e2af5e6bb421eb77d85be4387e6b
Archita <architapatelis@gmail.com>
2017-04-17 21:16:50 -0700

    error sponsored posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   # 'resources' method tells Rails to create post routes for creating, updating, viewing, deleting instances of Post.
   resources :topics do
     #nest the post routes under the topic routes
     resources :posts, except: [:index]
+    resources :sponsored_posts, except: [:index]
   end
 
   # path will be /about rather than /welcome/about
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,8 +38,20 @@ posts = Post.all
   )
 end
 
+20.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_integer
+  )
+end
+
+sponsored_posts = SponsoredPost.all
+
 
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
