90bb4f360aafbb85bf387cc047061ed64fa425ae
Archita <architapatelis@gmail.com>
2017-04-20 18:21:21 -0700

    add user avatar feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,10 @@ module ApplicationHelper
     # creates the symbol-specified HTML tag with the block contents, and if specified, the options hash
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+ end
+ 
 end
