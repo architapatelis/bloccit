dd798585dbc90dd9b2b62d64ffee4153e222f46f
Archita <architapatelis@gmail.com>
2017-04-21 17:37:55 -0700

    add scope to Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,11 @@ class Post < ActiveRecord::Base
   # default_scope will order all posts by their created_at date, in descending order, with the most recent posts displayed first.
   default_scope { order('created_at DESC') }
 
+  #list post in alphabetical order
+  scope :ordered_by_title, -> {order('title DESC')}
+
+  #opposite of default_scope
+  scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
