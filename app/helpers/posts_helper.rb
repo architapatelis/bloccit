b7885ee64229c7b26a6f81d0b96e117797d5277e
Archita <architapatelis@gmail.com>
2017-04-23 22:59:13 -0700

    fix error when guest views posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,6 +4,5 @@ module PostsHelper
 
   # checks if there is a signed-in current_user, and if that  current_user either owns the post, or is an admin.
   def user_is_authorized_for_post?(post)
-    current_user && (current_user == post.user) || current_user.admin?
-  end
+    current_user && (current_user == post.user || current_user.admin?)  end
 end
