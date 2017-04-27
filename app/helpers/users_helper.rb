177c438bd9732c4e77dc7e74a6a838b8adc108ed
Archita <architapatelis@gmail.com>
2017-04-26 21:51:52 -0700

    list user's favorite topics in show view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def user_has_no_posts?(user)
+    user && user.posts.count == 0
+  end
+
+  def user_has_no_comments?(user)
+    user && user.comments.count == 0
+  end
+
+  def user_has_favorites?(user)
+    user.favorites.count == 0
+  end
+
 end
